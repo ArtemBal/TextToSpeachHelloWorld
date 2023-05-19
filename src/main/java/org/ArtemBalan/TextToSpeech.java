0fea3ab8fb0bc2edbfdfdce09f9c83244c17ee8e
Artem Balan <a.s.balan96@gmail.com>
2023-05-19 22:46:40 +0300

    Create Synthesizer to say Hello, world!

--- src/main/java/org/ArtemBalan/TextToSpeech.java
+++ src/main/java/org/ArtemBalan/TextToSpeech.java
@@ -1,4 +1,44 @@
 package org.ArtemBalan;
 
+import java.util.Locale;
+import javax.speech.Central;
+import javax.speech.synthesis.Synthesizer;
+import javax.speech.synthesis.SynthesizerModeDesc;
+
 public class TextToSpeech {
+    public static void main(String[] args) {
+        try {
+            // Set property as Kevin Dictionary
+            System.setProperty(
+                    "freetts.voices",
+                    "com.sun.speech.freetts.en.us"
+                            + ".cmu_us_kal.KevinVoiceDirectory");
+
+            // Register Engine
+            Central.registerEngineCentral(
+                    "com.sun.speech.freetts"
+                            + ".jsapi.FreeTTSEngineCentral");
+
+            // Create a Synthesizer
+            Synthesizer synthesizer
+                    = Central.createSynthesizer(
+                    new SynthesizerModeDesc(Locale.US));
+
+            // Ready to speak
+            synthesizer.allocate();
+            synthesizer.resume();
+
+            // Speaks the given text until the queue is empty.
+            synthesizer.speakPlainText(
+                    "Hello, world!", null);
+            // wait until speaking is done and clean up
+            synthesizer.waitEngineState(
+                    Synthesizer.QUEUE_EMPTY);
+            synthesizer.deallocate();
+        }
+
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
